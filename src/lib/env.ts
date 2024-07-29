a1c434e86e56b1ab9212f904dd6ddbdd0b752162
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-07-29 20:07:59 -0300

    feat: add axios config

---
+++ src/lib/env.ts
@@ -0,0 +1,10 @@
+'use client';
+import { z } from 'zod';
+
+export const envSchema = z.object({
+	NEXT_PUBLIC_API_URL: z.string(),
+});
+
+export const env = envSchema.parse({
+	NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+});
