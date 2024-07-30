e32714056b85f5254089925b70e31ec45723e0b1
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-07-30 10:42:46 -0300

    feat: add favicon and set default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
                 <SessionProvider>
                     <ThemeProvider
                         attribute='class'
-                        defaultTheme='system'
+                        defaultTheme='light'
                         enableSystem
                         disableTransitionOnChange
                     >
