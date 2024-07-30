4a65f76102c22b94d73268bf98cf8eca8ba4d6d2
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-07-29 23:20:49 -0300

    feat: add new context to store user session ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,16 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { ReactQueryProvider } from '@/providers/react-query-provider';
+import { SessionProvider } from '@/contexts/SessionContext';
+import Header from '@/components/header';
+import { Toaster } from 'sonner';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'Desafio RevoluTI  - Gabriel Feijó',
 	description:
-		'An app for searching and validating postal codes, providing precise and up-to-date results in real-time, focusing on efficiency and intuitiveness.',
+		'Um aplicativo para pesquisa e validação de códigos postais, fornecendo resultados precisos e atualizados em tempo real, com foco na eficiência e intuitividade.',
 };
 
 export default function RootLayout({
@@ -20,14 +23,20 @@ export default function RootLayout({
 	return (
 		<html lang='pt-BR'>
 			<body className={inter.className}>
-				<ThemeProvider
-					attribute='class'
-					defaultTheme='system'
-					enableSystem
-					disableTransitionOnChange
-				>
-					<ReactQueryProvider>{children}</ReactQueryProvider>
-				</ThemeProvider>
+				<SessionProvider>
+					<ThemeProvider
+						attribute='class'
+						defaultTheme='system'
+						enableSystem
+						disableTransitionOnChange
+					>
+						<ReactQueryProvider>
+							<Header />
+							{children}
+							<Toaster richColors />
+						</ReactQueryProvider>
+					</ThemeProvider>
+				</SessionProvider>
 			</body>
 		</html>
 	);
---
+++ src/contexts/SessionContext.tsx
@@ -0,0 +1,76 @@
+'use client';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const SESSION_KEY = 'userSession';
+
+interface SessionContextProps {
+	sessionId: string | null;
+	createSession: () => void;
+	clearSession: () => void;
+}
+
+const SessionContext = createContext<SessionContextProps | undefined>(
+	undefined
+);
+
+const getSession = () =>
+	typeof window !== 'undefined'
+		? window.localStorage.getItem(SESSION_KEY)
+		: null;
+
+const saveSession = (sessionId: string) =>
+	typeof window !== 'undefined' &&
+	window.localStorage.setItem(SESSION_KEY, sessionId);
+
+const clearSessionStorage = () => {
+	typeof window !== 'undefined' && window.localStorage.removeItem(SESSION_KEY);
+};
+
+const SessionProvider: React.FC<{ children: React.ReactNode }> = ({
+	children,
+}) => {
+	const [sessionId, setSessionId] = useState<string | null>(null);
+
+	useEffect(() => {
+		const existingSession = getSession();
+		if (existingSession) {
+			setSessionId(existingSession);
+		} else {
+			createSession();
+		}
+	}, []);
+
+	const createSession = () => {
+		const newSessionId = uuidv4();
+		saveSession(newSessionId);
+		setSessionId(newSessionId);
+	};
+
+	const clearSession = () => {
+		clearSessionStorage();
+		setSessionId(null);
+	};
+
+	return (
+		<SessionContext.Provider
+			value={{
+				sessionId,
+				createSession,
+				clearSession,
+			}}
+		>
+			{children}
+		</SessionContext.Provider>
+	);
+};
+
+const useSession = () => {
+	const context = useContext(SessionContext);
+	if (context === undefined) {
+		throw new Error('useSession must be used within a SessionProvider');
+	}
+	return context;
+};
+
+export { SessionProvider, useSession };
