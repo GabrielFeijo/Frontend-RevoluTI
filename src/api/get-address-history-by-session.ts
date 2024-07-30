8567dfcee1f393cede346e2103e1b3fa8ee36a5f
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-07-29 23:22:26 -0300

    feat: add functions to make API requests

---
+++ src/api/get-address-history-by-session.ts
@@ -0,0 +1,11 @@
+import Address from '@/interfaces/address';
+import { api } from '@/lib/axios';
+
+export async function getAddressHistoryBySessionId({
+	sessionId,
+}: {
+	sessionId: string;
+}) {
+	const response = await api.get<Address[]>(`/address/session/${sessionId}`);
+	return response.data;
+}
---
+++ src/interfaces/address.ts
@@ -0,0 +1,12 @@
+interface Address {
+	id: string;
+	street: string;
+	postalCode: string;
+	city: string;
+	state: string;
+	country: string;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+export default Address;
